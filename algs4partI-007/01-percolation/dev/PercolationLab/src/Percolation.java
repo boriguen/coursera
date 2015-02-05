62582100f5499bd8bca59948a803959a4a5ba436
boriguen <boris.guenebaut@gmail.com>
2015-02-04 23:13:03 -0800

    Adding algs4partI-007 week 1 programming assignment skeleton related to percolation.

---
+++ algs4partI-007/01-percolation/dev/PercolationLab/src/Percolation.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+
+/**
+ * @author boris
+ *
+ */
+public class Percolation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Creates N-by-N grid, with all sites blocked.
+	 * @param N
+	 */
+	public Percolation(int N) {
+		
+	}
+	
+	/**
+	 * Opens site (row i, column j) if it is not open already.
+	 * @param i
+	 * @param j
+	 */
+	public void open(int i, int j) {
+		
+	}
+	
+	/**
+	 * Is site (row i, column j) open?
+	 * @param i
+	 * @param j
+	 * @return
+	 */
+	public boolean isOpen(int i, int j) {
+		return false;
+	}
+	
+	/**
+	 * Is site (row i, column j) full?
+	 * @param i
+	 * @param j
+	 * @return
+	 */
+	public boolean isFull(int i, int j) {
+		return false;
+	}
+	
+	/**
+	 * Does the system percolate?
+	 * @return
+	 */
+	public boolean percolates() {
+		return false;
+	}
+
+}
---
+++ algs4partI-007/01-percolation/dev/PercolationLab/src/PercolationStats.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+
+/**
+ * @author boris
+ *
+ */
+public class PercolationStats {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Performs T independent experiments on an N-by-N grid.
+	 * @param N
+	 * @param T
+	 */
+	public PercolationStats(int N, int T) {
+		
+	}
+	/**
+	 * Provides sample mean of percolation threshold.
+	 * @return
+	 */
+	public double mean() {
+		return 0;
+	}
+	
+	/**
+	 * Provides sample standard deviation of percolation threshold.
+	 * @return
+	 */
+	public double stddev() {
+		return 0;
+	}
+	
+	/**
+	 * Provides low  endpoint of 95% confidence interval.
+	 * @return
+	 */
+	public double confidenceLo() {
+		return 0;
+	}
+	
+	/**
+	 * Provides high endpoint of 95% confidence interval.
+	 * @return
+	 */
+	public double confidenceHi() {
+		return 0;
+	}
+
+}
