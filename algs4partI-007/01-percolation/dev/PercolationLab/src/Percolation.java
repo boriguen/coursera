b737cce22c9f290fab6cbca2a7250865bfe02fe9
boriguen <boris.guenebaut@gmail.com>
2015-02-08 19:43:45 -0800

    Updating source code after second submission attempt.

--- algs4partI-007/01-percolation/dev/PercolationLab/src/Percolation.java
+++ algs4partI-007/01-percolation/dev/PercolationLab/src/Percolation.java
@@ -1,4 +1,3 @@
-
 /**
  * 
  */
@@ -8,166 +7,181 @@
  *
  */
 public class Percolation {
-	
-	private int N = 0;
-	private int[] grid = null;
-	private int virtualTopSiteIndex = 0;
-	private int virtualBottomSiteIndex = 0;
-	private WeightedQuickUnionUF weightedQuickUnionUF = null;
-	
-	/**
-	 * Creates N-by-N grid, with all sites blocked.
-	 * @param N
-	 */
-	public Percolation(int N) {
-		if (N <= 0) {
-			throw new IllegalArgumentException("N must be greater than 0");
-		}
-		this.N = N;
-		
-		this.grid = new int[N * N];
-		for (int i = 0; i < this.grid.length; i++) {
-			this.grid[i] = 0;
-		}
-		
-		initWeightedQuickUnion();
-	}
-	
-	/**
-	 * Opens site (row i, column j) if it is not open already.
-	 * @param i
-	 * @param j
-	 */
-	public void open(int i, int j) {
-		int unifiedIndex = processUnifiedIndex(i, j);
-		
-		if (this.grid[unifiedIndex] != 1) {
-			// Open site.
-			this.grid[unifiedIndex] = 1;
-			
-			if (i == 1) {
-				this.weightedQuickUnionUF.union(this.virtualTopSiteIndex, unifiedIndex);
-			} else if (i == N) {
-				this.weightedQuickUnionUF.union(this.virtualBottomSiteIndex, unifiedIndex);
-			}
-			
-			// Union between neighboring sites as applicable.
-			trySiteUnion(unifiedIndex, i-1, j); // Top.
-			trySiteUnion(unifiedIndex, i+1, j); // Bottom.
-			trySiteUnion(unifiedIndex, i, j-1); // Left.
-			trySiteUnion(unifiedIndex, i, j+1); // Right.
-		}
-	}
-	
-	/**
-	 * Is site (row i, column j) open?
-	 * @param i
-	 * @param j
-	 * @return
-	 */
-	public boolean isOpen(int i, int j) {
-		return isOpen(processUnifiedIndex(i, j));
-	}
-	
-	/**
-	 * Is site (row i, column j) full?
-	 * @param i
-	 * @param j
-	 * @return
-	 */
-	public boolean isFull(int i, int j) {
-		return isFull(processUnifiedIndex(i, j));
-	}
-	
-	/**
-	 * Does the system percolate?
-	 * @return
-	 */
-	public boolean percolates() {
-		return this.weightedQuickUnionUF.connected(this.virtualTopSiteIndex, 
-				this.virtualBottomSiteIndex);
-	}
-	
-	/**
-	 * Is site (unifiedIndex) open?
-	 * @param unifiedIndex
-	 * @return
-	 */
-	private boolean isOpen(int unifiedIndex) {
-		return this.grid[unifiedIndex] == 1;
-	}
-	
-	/**
-	 * Is site (unifiedIndex) full?
-	 * @param unifiedIndex
-	 * @return
-	 */
-	private boolean isFull(int unifiedIndex) {
-		return isOpen(unifiedIndex) && this.weightedQuickUnionUF.connected(unifiedIndex,
-				this.virtualTopSiteIndex);
-	}
-	
-	/**
-	 * Prints the current state of the system to the console.
-	 */
-	private void printSystem() {
-		System.out.println("\n\nCurrent percolation system state:");
-		System.out.print(this.grid[0]);
-		for (int i = 1; i < this.grid.length; i++) {
-			if (i % this.N == 0) {
-				System.out.println();
-			} else {
-				System.out.print(' ');
-			}
-			System.out.print(this.grid[i]);
-		}
-	}
-	
-	private void initWeightedQuickUnion() {
-		int N2 = this.N * this.N;
-		
-		// Determine virtual site indexes.
-		this.virtualTopSiteIndex = N2;
-		this.virtualBottomSiteIndex = N2 + 1;
-		
-		// Instantiate the weighted quick union.
-		this.weightedQuickUnionUF = new WeightedQuickUnionUF(N2 + 2);
-	}
-	
-	/**
-	 * Checks whether the given indexes are within the bounds and calculates
-	 * unified index to be used internally.
-	 * @param i
-	 * @param j
-	 */
-	private int processUnifiedIndex(int i, int j) {
-		final String template = "Index %s of value %d is out of bounds"; 
-		if (i < 1 || i > this.N) {
-			throw new IndexOutOfBoundsException(String.format(template, "i", i));
-		} else if (j < 1 || j > this.N) {
-			throw new IndexOutOfBoundsException(String.format(template, "j", j));
-		}
-		return (i-1) * this.N + (j-1);
-	}
-	
-	/**
-	 * Tries to perform a site union between a given unified index site and 
-	 * a site of indexes i, j.
-	 * @param unifiedIndex
-	 * @param i
-	 * @param j
-	 * @return
-	 */
-	private boolean trySiteUnion(int unifiedIndex, int i, int j) {
-		boolean success = false;
-		if (i >= 1 && i <= this.N && j >= 1 && j <= this.N) {
-			int neighborUnifiedIndex = processUnifiedIndex(i, j);
-			if (isOpen(neighborUnifiedIndex)) {
-				this.weightedQuickUnionUF.union(unifiedIndex, neighborUnifiedIndex);
-				success = true;
-			}
-		}
-		return success;
-	}
+
+    private int N = 0;
+    private int[] grid = null;
+    private int virtualTopSiteIndex = 0;
+    private int virtualBottomSiteIndex = 0;
+    private WeightedQuickUnionUF weightedQuickUnionUF = null;
+
+    /**
+     * Creates N-by-N grid, with all sites blocked.
+     * 
+     * @param N
+     */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N must be greater than 0");
+        }
+        this.N = N;
+
+        this.grid = new int[N * N];
+        for (int i = 0; i < this.grid.length; i++) {
+            this.grid[i] = 0;
+        }
+
+        initWeightedQuickUnion();
+    }
+
+    /**
+     * Opens site (row i, column j) if it is not open already.
+     * 
+     * @param i
+     * @param j
+     */
+    public void open(int i, int j) {
+        int unifiedIndex = processUnifiedIndex(i, j);
+
+        if (this.grid[unifiedIndex] != 1) {
+            // Open site.
+            this.grid[unifiedIndex] = 1;
+
+            if (i == 1) {
+                this.weightedQuickUnionUF.union(this.virtualTopSiteIndex,
+                        unifiedIndex);
+            }
+
+            if (i == N) {
+                this.weightedQuickUnionUF.union(this.virtualBottomSiteIndex,
+                        unifiedIndex);
+            }
+
+            // Union between neighboring sites as applicable.
+            trySiteUnion(unifiedIndex, i - 1, j); // Top.
+            trySiteUnion(unifiedIndex, i + 1, j); // Bottom.
+            trySiteUnion(unifiedIndex, i, j - 1); // Left.
+            trySiteUnion(unifiedIndex, i, j + 1); // Right.
+        }
+    }
+
+    /**
+     * Is site (row i, column j) open?
+     * 
+     * @param i
+     * @param j
+     * @return
+     */
+    public boolean isOpen(int i, int j) {
+        return isOpen(processUnifiedIndex(i, j));
+    }
+
+    /**
+     * Is site (row i, column j) full?
+     * 
+     * @param i
+     * @param j
+     * @return
+     */
+    public boolean isFull(int i, int j) {
+        return isFull(processUnifiedIndex(i, j));
+    }
+
+    /**
+     * Does the system percolate?
+     * 
+     * @return
+     */
+    public boolean percolates() {
+        return this.weightedQuickUnionUF.connected(this.virtualTopSiteIndex,
+                this.virtualBottomSiteIndex);
+    }
+
+    /**
+     * Is site (unifiedIndex) open?
+     * 
+     * @param unifiedIndex
+     * @return
+     */
+    private boolean isOpen(int unifiedIndex) {
+        return this.grid[unifiedIndex] == 1;
+    }
+
+    /**
+     * Is site (unifiedIndex) full?
+     * 
+     * @param unifiedIndex
+     * @return
+     */
+    private boolean isFull(int unifiedIndex) {
+        return isOpen(unifiedIndex)
+                && this.weightedQuickUnionUF.connected(unifiedIndex,
+                        this.virtualTopSiteIndex);
+    }
+
+    /**
+     * Prints the current state of the system to the console.
+     */
+    private void printSystem() {
+        System.out.println("\n\nCurrent percolation system state:");
+        System.out.print(this.grid[0]);
+        for (int i = 1; i < this.grid.length; i++) {
+            if (i % this.N == 0) {
+                System.out.println();
+            } else {
+                System.out.print(' ');
+            }
+            System.out.print(this.grid[i]);
+        }
+    }
+
+    private void initWeightedQuickUnion() {
+        int N2 = this.N * this.N;
+
+        // Determine virtual site indexes.
+        this.virtualTopSiteIndex = N2;
+        this.virtualBottomSiteIndex = N2 + 1;
+
+        // Instantiate the weighted quick union.
+        this.weightedQuickUnionUF = new WeightedQuickUnionUF(N2 + 2);
+    }
+
+    /**
+     * Checks whether the given indexes are within the bounds and calculates
+     * unified index to be used internally.
+     * 
+     * @param i
+     * @param j
+     */
+    private int processUnifiedIndex(int i, int j) {
+        final String template = "Index %s of value %d is out of bounds";
+        if (i < 1 || i > this.N) {
+            throw new IndexOutOfBoundsException(String.format(template, "i", i));
+        } else if (j < 1 || j > this.N) {
+            throw new IndexOutOfBoundsException(String.format(template, "j", j));
+        }
+        return (i - 1) * this.N + (j - 1);
+    }
+
+    /**
+     * Tries to perform a site union between a given unified index site and a
+     * site of indexes i, j.
+     * 
+     * @param unifiedIndex
+     * @param i
+     * @param j
+     * @return
+     */
+    private boolean trySiteUnion(int unifiedIndex, int i, int j) {
+        boolean success = false;
+        if (i >= 1 && i <= this.N && j >= 1 && j <= this.N) {
+            int neighborUnifiedIndex = processUnifiedIndex(i, j);
+            if (isOpen(neighborUnifiedIndex)) {
+                this.weightedQuickUnionUF.union(unifiedIndex,
+                        neighborUnifiedIndex);
+                success = true;
+            }
+        }
+        return success;
+    }
 
 }
--- algs4partI-007/01-percolation/dev/PercolationLab/src/PercolationStats.java
+++ algs4partI-007/01-percolation/dev/PercolationLab/src/PercolationStats.java
@@ -10,154 +10,150 @@ import java.util.Random;
  */
 public class PercolationStats {
 
-	private double[] percolationSiteCounts = null;
+    private double[] percolationSiteCounts = null;
 
-	/**
-	 * Performs T independent experiments on an N-by-N grid.
-	 * 
-	 * @param N
-	 * @param T
-	 */
-	public PercolationStats(int N, int T) {
-		if (N <= 0) {
-			throw new IllegalArgumentException("N should be greater than 0");
-		}
+    /**
+     * Performs T independent experiments on an N-by-N grid.
+     * 
+     * @param N
+     * @param T
+     */
+    public PercolationStats(int N, int T) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N should be greater than 0");
+        }
 
-		if (T <= 0) {
-			throw new IllegalArgumentException("T should be greater than 0");
-		}
+        if (T <= 0) {
+            throw new IllegalArgumentException("T should be greater than 0");
+        }
 
-		// Perform experiments.
-		percolationSiteCounts = new double[T];
-		performExperiments(N, T);
-	}
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		if (args.length != 2) {
-			throw new IllegalArgumentException(
-					"The program should only contain N and T");
-		}
+        // Perform experiments.
+        percolationSiteCounts = new double[T];
+        performExperiments(N, T);
+    }
 
-		//Stopwatch stopWatch = new Stopwatch();
-		
-		// Compute stats.
-		int N = Integer.parseInt(args[0]);
-		int T = Integer.parseInt(args[1]);
-		PercolationStats percolationStats = new PercolationStats(N, T);
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException(
+                    "The program should only contain N and T");
+        }
 
-		// Display stats.
-		System.out.printf("mean                    = %f",
-				percolationStats.mean());
-		System.out.printf("\nstddev                  = %f",
-				percolationStats.stddev());
-		System.out.printf("\n95%% confidence interval = %f, %f",
-				percolationStats.confidenceLo(),
-				percolationStats.confidenceHi());
-		
-		/*System.out.printf("\n\nTotal execution time for N = %d and T = %d is %f seconds",
-				N, T, stopWatch.elapsedTime());*/
-	}
+        // Compute stats.
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        PercolationStats percolationStats = new PercolationStats(N, T);
 
-	/**
-	 * Provides sample mean of percolation threshold.
-	 * 
-	 * @return
-	 */
-	public double mean() {
-		return StdStats.mean(percolationSiteCounts);
-	}
+        // Display stats.
+        System.out.printf("mean                    = %f",
+                percolationStats.mean());
+        System.out.printf("\nstddev                  = %f",
+                percolationStats.stddev());
+        System.out.printf("\n95%% confidence interval = %f, %f",
+                percolationStats.confidenceLo(),
+                percolationStats.confidenceHi());
+    }
 
-	/**
-	 * Provides sample standard deviation of percolation threshold.
-	 * 
-	 * @return
-	 */
-	public double stddev() {
-		return StdStats.stddev(percolationSiteCounts);
-	}
+    /**
+     * Provides sample mean of percolation threshold.
+     * 
+     * @return
+     */
+    public double mean() {
+        return StdStats.mean(percolationSiteCounts);
+    }
 
-	/**
-	 * Provides low endpoint of 95% confidence interval.
-	 * 
-	 * @return
-	 */
-	public double confidenceLo() {
-		return mean() - 0.95 * stddev();
-	}
+    /**
+     * Provides sample standard deviation of percolation threshold.
+     * 
+     * @return
+     */
+    public double stddev() {
+        return StdStats.stddev(percolationSiteCounts);
+    }
 
-	/**
-	 * Provides high endpoint of 95% confidence interval.
-	 * 
-	 * @return
-	 */
-	public double confidenceHi() {
-		return mean() + 0.95 * stddev();
-	}
-	
-	/**
-	 * Returns a pseudo-random number between min and max, inclusive. The
-	 * difference between min and max can be at most
-	 * <code>Integer.MAX_VALUE - 1</code>.
-	 *
-	 * @param min
-	 *            Minimum value
-	 * @param max
-	 *            Maximum value. Must be greater than min.
-	 * @return Integer between min and max, inclusive.
-	 * @see java.util.Random#nextInt(int)
-	 */
-	private static int randInt(Random random, int min, int max) {
-		// nextInt is normally exclusive of the top value,
-		// so add 1 to make it inclusive
-		int randomNum = random.nextInt((max - min) + 1) + min;
+    /**
+     * Provides low endpoint of 95% confidence interval.
+     * 
+     * @return
+     */
+    public double confidenceLo() {
+        return mean() - 0.95 * stddev();
+    }
 
-		return randomNum;
-	}
+    /**
+     * Provides high endpoint of 95% confidence interval.
+     * 
+     * @return
+     */
+    public double confidenceHi() {
+        return mean() + 0.95 * stddev();
+    }
 
-	/**
-	 * Performs T percolation experiments of size N*N.
-	 * 
-	 * @param N
-	 * @param T
-	 */
-	private void performExperiments(int N, int T) {
-		for (int i = 0; i < T; i++) {
-			percolationSiteCounts[i] = performExperiment(N);
-		}
-	}
+    /**
+     * Returns a pseudo-random number between min and max, inclusive. The
+     * difference between min and max can be at most
+     * <code>Integer.MAX_VALUE - 1</code>.
+     *
+     * @param min
+     *            Minimum value
+     * @param max
+     *            Maximum value. Must be greater than min.
+     * @return Integer between min and max, inclusive.
+     * @see java.util.Random#nextInt(int)
+     */
+    private static int randInt(Random random, int min, int max) {
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = random.nextInt((max - min) + 1) + min;
 
-	/**
-	 * Performs a percolation experiment for a grid of size N*N and the threshold.
-	 * 
-	 * @param N
-	 * @return
-	 */
-	private double performExperiment(int N) {
-		Percolation percolation = new Percolation(N);
-		
-		Random random = new Random();
-		int count = 0;
-		int i = 0, j = 0;
-		int min = 1, max = N;
-		int maxSites = N * N;
-		do {
-			// Select random i and j.
-			i = randInt(random, min, max);
-			j = randInt(random, min, max);
-			
-			if (!percolation.isOpen(i, j)) {
-				// Open site.
-				percolation.open(i, j);
-				++count;
-			}
-		} while (!percolation.percolates() && count < maxSites);
+        return randomNum;
+    }
 
-		double threshold = (double) count / (double) maxSites;
-		
-		return threshold;
-	}
+    /**
+     * Performs T percolation experiments of size N*N.
+     * 
+     * @param N
+     * @param T
+     */
+    private void performExperiments(int N, int T) {
+        for (int i = 0; i < T; i++) {
+            percolationSiteCounts[i] = performExperiment(N);
+        }
+    }
+
+    /**
+     * Performs a percolation experiment for a grid of size N*N and the
+     * threshold.
+     * 
+     * @param N
+     * @return
+     */
+    private double performExperiment(int N) {
+        Percolation percolation = new Percolation(N);
+
+        Random random = new Random();
+        int count = 0;
+        int i = 0, j = 0;
+        int min = 1, max = N;
+        int maxSites = N * N;
+        do {
+            // Select random i and j.
+            i = randInt(random, min, max);
+            j = randInt(random, min, max);
+
+            if (!percolation.isOpen(i, j)) {
+                // Open site.
+                percolation.open(i, j);
+                ++count;
+            }
+        } while (!percolation.percolates() && count < maxSites);
+
+        double threshold = (double) count / (double) maxSites;
+
+        return threshold;
+    }
 
 }
